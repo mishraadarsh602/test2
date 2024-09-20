ef9750407f98ed5cff440fd6542ee60669c5f1d9
ashish <ashish.sat7@gmail.com>
2024-09-20 17:21:56 +0530

    Change

--- src/controllers/builder.js
+++ src/controllers/builder.js
@@ -43,10 +43,10 @@ module.exports = {
             if (userExist) {
                 const token = await userExist.generateToken();
                 res.cookie('token', token, {
-                    httpOnly: false, // Makes the cookie inaccessible to JavaScript (XSS protection)
+                    // httpOnly: false, // Makes the cookie inaccessible to JavaScript (XSS protection)
                     maxAge: 24 * 60 * 60 * 1000, // 24 hours
-                    sameSite: 'none',
-                    secure: process.env.NODE_ENV === 'production'
+                    // sameSite: 'none',
+                    // secure: process.env.NODE_ENV === 'production'
                 });
                 return res.status(201).json({
                     message: 'User already exists',
@@ -70,10 +70,10 @@ module.exports = {
             const token = await userCreated.generateToken();
     
             res.cookie('token', token, {
-                httpOnly: false, // Makes the cookie inaccessible to JavaScript (XSS protection)
+                // httpOnly: false, // Makes the cookie inaccessible to JavaScript (XSS protection)
                 maxAge: 24 * 60 * 60 * 1000, // 24 hours
-                sameSite: 'none',
-                secure: process.env.NODE_ENV === 'production'
+                // sameSite: 'none',
+                // secure: process.env.NODE_ENV === 'production'
             });
     
             res.status(201).json({
