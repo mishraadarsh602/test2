8e167e6c6b982e4265b06aa354866a940171609d
ashish <ashish.sat7@gmail.com>
2024-09-22 17:57:47 +0530

    AI

--- src/controllers/ai.js
+++ src/controllers/ai.js
@@ -1,18 +1,21 @@
 const systemPromptSession = require('../models/chat/systemPrompt.model');
 const App = require('../models/app');
 const { Anthropic } = require('@anthropic-ai/sdk');
-
+const { OpenAI } = require("openai");
+const fs = require('fs');
+const path = require('path');
+const openai = new OpenAI({ apiKey: process.env.OPEN_AI_KEY });
 const client = new Anthropic({
-    apiKey: process.env['ANTHROPIC_API_KEY'],
+  apiKey: process.env['ANTHROPIC_API_KEY'],
 });
 module.exports = {
 
-    createAppByAI: async (req, res) => {
-        try {
-            let aiData = req.body;
-            const prompts = await systemPromptSession.findOne({});
-            let parentPrompt = prompts?.parentPrompt;
-            let apiList = `[
+  createAppByAI: async (req, res) => {
+    try {
+      let aiData = req.body;
+      const prompts = await systemPromptSession.findOne({});
+      let parentPrompt = prompts?.parentPrompt;
+      let apiList = `[
                 {
                     API: 'http://api.weatherapi.com/v1/forecast.json?q=gurgaon&key=YOUR_API&days=2',
                     Purpose: 'To retrieve the 2-day weather forecast (current day and next day) for a specific location, including hourly updates.',
@@ -25,60 +28,166 @@ module.exports = {
                 }
             ]`
 
-            parentPrompt = parentPrompt.replace('{userInput}', aiData.customPrompt);
-            parentPrompt = parentPrompt.replace('{apiList}', apiList);
-            // console.log("parentPrompt", parentPrompt)
-            const message = await client.messages.create({
-                max_tokens: 1024,
-                messages: [{ role: 'user', content: parentPrompt }],
-                model: 'claude-3-5-sonnet-20240620',
-            });
-            let parentResponse = JSON.parse(message.content[0].text.trim());
-            console.log(parentResponse);
+      parentPrompt = parentPrompt.replace('{userInput}', aiData.customPrompt);
+      parentPrompt = parentPrompt.replace('{apiList}', apiList);
+      // console.log("parentPrompt", parentPrompt)
+      const message = await client.messages.create({
+        max_tokens: 1024,
+        messages: [{ role: 'user', content: parentPrompt }],
+        model: 'claude-3-5-sonnet-20240620',
+      });
+      let parentResponse = JSON.parse(message.content[0].text.trim());
+      console.log(parentResponse);
 
-            let waitForChildOperation = await CALLAI(parentResponse,prompts,aiData);
+      let waitForChildOperation = await CALLAI(parentResponse, prompts, aiData);
 
-            res.status(200).json({
-                message: "Prompt Chaining",
-                data: waitForChildOperation,
-            });
-        } catch (error) {
-            res.status(400).json({ error: error.message });
-        }
-    },
-
-    callAPI: async (req, res) => { // get called from live APP( as live app dont have api only parentApp have)
-        try {
-            let body = req.body;
-            const app = await App.findOne({ parentApp: body.parentApp });
-            const response = await fetch(`${app.api}`);
-            const data = await response.json();
-            res.status(200).json({
-                message: "API Called",
-                data,
-            });
-        } catch (error) {
-            res.status(400).json({ error: error.message });
-        }
-    },
-
-    returnCode: async (req, res) => { // get called from live APP( as live app dont have api only parentApp have)
-        try {
-            const app = await App.findOne({ _id: "66e2cbe8ecccb0a4162b2b0c" });
-            console.log("ashish",app['componentCode'])
-            res.status(200).json({
-                code: app['componentCode']
-            });
-        } catch (error) {
-            res.status(400).json({ error: error.message });
-        }
+      res.status(200).json({
+        message: "Prompt Chaining",
+        data: waitForChildOperation,
+      });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
     }
+  },
+
+  callAPI: async (req, res) => { // get called from live APP( as live app dont have api only parentApp have)
+    try {
+      let body = req.body;
+      const app = await App.findOne({ parentApp: body.parentApp });
+      const response = await fetch(`${app.api}`);
+      const data = await response.json();
+      res.status(200).json({
+        message: "API Called",
+        data,
+      });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  },
+
+  returnCode: async (req, res) => { // get called from live APP( as live app dont have api only parentApp have)
+    try {
+      const app = await App.findOne({ _id: "66e2cbe8ecccb0a4162b2b0c" });
+      console.log("ashish", app['componentCode'])
+      res.status(200).json({
+        code: app['componentCode']
+      });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  },
+
+  createAssistant: async (req, res) => {
+    try {
+      const assistant = await openai.beta.assistants.create({
+        name: "AI Assistant",
+        instructions: `You are a decision-maker. I want to build a tool. Based on my input, you will decide whether the context requires some APIs to perform actions or if it's a general AI-based requirement that doesn’t require any APIs or just a general text. 
+        If APIs are required return: \n'ToolTYPE': 'APIBASED'\n If the requirement is general AI-based and does not require APIs, return: \n'ToolTYPE': 'AIBASED'.\nIf it is general or random text, return:\n'ToolTYPE':'GENERALTEXT'.
+        Your response should follow this JSON structure: {\"ToolTYPE\": \"\"}`,
+        response_format: { type: "json_object" },
+        model: "gpt-4o-mini"
+      });
+      console.log("assistant", assistant);
+      res.status(200).json({
+        assistant: assistant
+      });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  },
+
+
+  runAssistantConversation: async () => {
+    try {
+      const threadId = 'thread_fS0zR54kRicI2cke6Wl1tIjW';
+      const userMessage = 'what was my 1st message';
+      await addMessageToThread(threadId, userMessage);
+
+      // const imagePath = './path-to-image.jpg'; // Replace with your image file path
+      // await addImageToThread(threadId, imagePath);
+
+      console.log('Running assistant on thread...');
+      await runAssistantOnThread(threadId);
+
+    } catch (error) {
+      console.error('Error in assistant conversation:', error);
+    }
+  }
 }
 
-async function CALLAI(parentResponse, prompts,aiData) {
-    if (parentResponse && parentResponse.ToolTYPE === 'AIBASED') {
-        let childPrompt = prompts?.childPrompt?.aibased;
-        let reactCode = `
+addMessageToThread = async (threadId, messageText) => {
+  try {
+    const message = await openai.beta.threads.messages.create(
+      threadId,
+      {
+        role: "user",
+        content: messageText
+      }
+    );
+
+    console.log('Message Added:', message);
+    return message.data;
+  } catch (error) {
+    console.error('Error adding message to thread:', error);
+  }
+};
+
+addImageToThread = async (threadId, imagePath) => {
+  try {
+    // Read the image file
+    const imageFile = fs.createReadStream(path.resolve(imagePath));
+
+    // Upload the image file and add it to the conversation
+    const response = await openai.beta.threads.messages.create(
+      threadId,
+      {
+        role: "user",
+        content: imageFile
+      }
+    );
+    console.log('Image Added:', response.data);
+    return response.data;
+  } catch (error) {
+    console.error('Error adding image to thread:', error);
+  }
+};
+
+runAssistantOnThread = async (threadId) => {
+  try {
+    const run = await openai.beta.threads.runs.stream(
+      threadId,
+      { assistant_id: process.env.PARENT_ASSISTANT_ID }
+    )
+    // .on('textCreated', (text) => process.stdout.write('\nassistant > '))
+    .on('textDelta', (textDelta, snapshot) => process.stdout.write(textDelta.value))
+    // .on('toolCallCreated', (toolCall) => process.stdout.write(`\nassistant > ${toolCall.type}\n\n`))
+    // .on('toolCallDelta', (toolCallDelta, snapshot) => {
+    //   if (toolCallDelta.type === 'code_interpreter') {
+    //     if (toolCallDelta.code_interpreter.input) {
+    //       process.stdout.write(toolCallDelta.code_interpreter.input);
+    //     }
+    //     if (toolCallDelta.code_interpreter.outputs) {
+    //       process.stdout.write('\noutput >\n');
+    //       toolCallDelta.code_interpreter.outputs.forEach((output) => {
+    //         if (output.type === 'logs') {
+    //           process.stdout.write(`\n${output.logs}\n`);
+    //         }
+    //       });
+    //     }
+    //   }
+    // });
+    const finalFunctionCall = await run.finalMessages();
+    console.log('Run end:',finalFunctionCall);
+    return run;
+  } catch (error) {
+    console.error('Error running assistant:', error);
+  }
+};
+
+async function CALLAI(parentResponse, prompts, aiData) {
+  if (parentResponse && parentResponse.ToolTYPE === 'AIBASED') {
+    let childPrompt = prompts?.childPrompt?.aibased;
+    let reactCode = `
         function WeatherApp() {
           const [weather, setWeather] = React.useState(null);
           const [loading, setLoading] = React.useState(true);
@@ -141,29 +250,29 @@ async function CALLAI(parentResponse, prompts,aiData) {
       
         return WeatherApp;
       `;
-        childPrompt = childPrompt.replace('{userInput}', aiData.customPrompt);
-        childPrompt = childPrompt.replace('{reactCode}', reactCode);
-        const mesg = await client.messages.create({
-            max_tokens: 8192,
-            messages: [{ role: 'user', content: childPrompt }],
-            model: 'claude-3-5-sonnet-20240620',
-        });
-        let childResponse = mesg.content[0].text.trim();
-        console.log(childResponse);
-        // let message = (childResponse).split("@$@$@$");
-        // let obj = { code: message[0], message: message[1] };
-        const app = await App.findOne({ _id: "66e2cbe8ecccb0a4162b2b0c" });
-        app.componentCode = childResponse;
-        await app.save()
-        return app;
-    }
-    else if (parentResponse && parentResponse.ToolTYPE === 'APIBASED') {
-        let childPrompt = prompts?.childPrompt?.apibased;
-        childPrompt = childPrompt.replace('{userInput}', aiData.customPrompt);
-        // console.log(childPrompt)
-        const app = await App.findOne({ _id: "66e2cbe8ecccb0a4162b2b0c" });
-        // app.componentCode = childResponse;
-        // await app.save()
-        return app;
-    }
+    childPrompt = childPrompt.replace('{userInput}', aiData.customPrompt);
+    childPrompt = childPrompt.replace('{reactCode}', reactCode);
+    const mesg = await client.messages.create({
+      max_tokens: 8192,
+      messages: [{ role: 'user', content: childPrompt }],
+      model: 'claude-3-5-sonnet-20240620',
+    });
+    let childResponse = mesg.content[0].text.trim();
+    console.log(childResponse);
+    // let message = (childResponse).split("@$@$@$");
+    // let obj = { code: message[0], message: message[1] };
+    const app = await App.findOne({ _id: "66e2cbe8ecccb0a4162b2b0c" });
+    app.componentCode = childResponse;
+    await app.save()
+    return app;
+  }
+  else if (parentResponse && parentResponse.ToolTYPE === 'APIBASED') {
+    let childPrompt = prompts?.childPrompt?.apibased;
+    childPrompt = childPrompt.replace('{userInput}', aiData.customPrompt);
+    // console.log(childPrompt)
+    const app = await App.findOne({ _id: "66e2cbe8ecccb0a4162b2b0c" });
+    // app.componentCode = childResponse;
+    // await app.save()
+    return app;
+  }
 }
