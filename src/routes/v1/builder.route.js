556a1f61d6b87d41753e54e0986e547c459fb32c
ashish <ashish.sat7@gmail.com>
2024-11-27 00:38:49 +0530

    AI

--- src/routes/v1/builder.route.js
+++ src/routes/v1/builder.route.js
@@ -14,7 +14,6 @@ router.post('/create_user', builderController.createUser);
 router.get('/get_user',auth, builderController.getUserDetail);
 router.post('/create_app_by_AI', aiController.createAppByAI);
 router.get('/create_assistant', aiController.createAssistant);
-router.get('/get_assistant_conversatation', aiController.runAssistantConversation);
 router.get('/generate_graph', aiController.tryGraphMaking);
 router.get('/returnCode',aiController.returnCode);
 router.get('/fetchAssistant', aiController.getAssistantAI);
